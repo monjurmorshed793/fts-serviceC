a5860178d96ee5362d238d493f817f6f8635d3b6
monju <monjurmorshed793@gmail.com>
2018-02-11 01:24:17 +0600

    Parallel process added.

--- src/main/java/org/ums/servicec/ServicecApplication.java
+++ src/main/java/org/ums/servicec/ServicecApplication.java
@@ -9,8 +9,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.websocket.server.PathParam;
 
 @SpringBootApplication
+@RestController("/")
 public class ServicecApplication {
     public static Logger logger = LoggerFactory.getLogger(ServicecApplication.class);
     @Autowired
@@ -20,20 +26,31 @@ public class ServicecApplication {
 		SpringApplication.run(ServicecApplication.class, args);
 	}
 
-  @KafkaListener(topics = "my_topic")
+  @GetMapping("/service-c")
+  public boolean getReponse()throws Exception{
+    RestTemplate restTemplate = new RestTemplate();
+    Thread.sleep(500);
+    return true;
+  }
+
+  @KafkaListener(topics = "service-c")
   public void listen(ConsumerRecord<?, ?> cr) throws Exception {
-	    if(cr.key().toString().equals("serviceC")){
+
             logger.info(cr.key().toString());
             logger.info(cr.value().toString());
-        ObjectMapper mapper = new ObjectMapper();
-        ServiceStatus receivedServiceStatus = mapper.readValue(cr.value().toString(), ServiceStatus.class);
-        ServiceStatus serviceStatus = new ServiceStatus();
-        serviceStatus.setParentServiceId(receivedServiceStatus.getServiceId());
-        serviceStatus.setServiceId("serviceC");
-        serviceStatus.setStatus(true);
-            String jsonObject = mapper.writeValueAsString(mapper);
-            template.send("tracker", "serviceC", jsonObject);
-        }
+    for(int i=1;i<=Integer.parseInt(cr.key().toString());i++){
+      ObjectMapper mapper = new ObjectMapper();
+      ServiceStatus receivedServiceStatus = mapper.readValue(cr.value().toString(), ServiceStatus.class);
+      ServiceStatus serviceStatus = new ServiceStatus();
+      serviceStatus.setParentServiceId(receivedServiceStatus.getServiceId());
+      serviceStatus.setServiceId("serviceC"+i);
+      serviceStatus.setStatus(true);
+      String jsonObject = mapper.writeValueAsString(serviceStatus);
+      template.send("tracker", "serviceC", jsonObject);
+    }
+
+    Thread.sleep(500);
+
 
     }
 }
