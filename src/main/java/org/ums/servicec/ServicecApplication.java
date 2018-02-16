464baf03770ccdad0840f683dbffc94986d96d5f
monju <monjurmorshed793@gmail.com>
2018-02-17 01:04:23 +0600

    Hystrix added.

--- src/main/java/org/ums/servicec/ServicecApplication.java
+++ src/main/java/org/ums/servicec/ServicecApplication.java
@@ -1,12 +1,14 @@
 package org.ums.servicec;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,23 +16,41 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.websocket.server.PathParam;
+import java.util.List;
 
 @SpringBootApplication
 @RestController("/")
+@EnableCircuitBreaker
 public class ServicecApplication {
     public static Logger logger = LoggerFactory.getLogger(ServicecApplication.class);
     @Autowired
     private KafkaTemplate<String, String> template;
+  @Autowired
+  CheckinsRepository mCheckinsRepository;
+  @Autowired
+  NativePartitionService mNativePartitionService;
+  @Autowired
+  PartitionRepository mPartitionRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServicecApplication.class, args);
 	}
 
   @GetMapping("/service-c")
+  @HystrixCommand(fallbackMethod = "reliable")
   public boolean getReponse()throws Exception{
     RestTemplate restTemplate = new RestTemplate();
-    Thread.sleep(500);
+    Iterable<Checkins> checkins = mCheckinsRepository.findAll();
+    List<Checkins> checkinsList = CrowdSourceUtils.convertFromIterableToList(checkins);
+    checkinsList.sort((o1,o2)->o1.getDate().compareTo(o2.getDate()));
+    //Map<Date, List<Checkins>> listMap = mNativePartitionService.getGroupBy(checkinsList);
+
+    mNativePartitionService.assignPartitions(checkinsList, 500);
     return true;
+  }
+
+  public boolean reliable(){
+    return false;
   }
 
   @KafkaListener(topics = "service-c")
